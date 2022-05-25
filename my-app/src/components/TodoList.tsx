293813d42840e0d9709b98298c08051a4b88e58f
yamadamasashi <shisama94@gmail.com>
2022-05-25 22:03:30 +0900

    recoil

--- my-app/src/components/TodoList.tsx
+++ my-app/src/components/TodoList.tsx
@@ -26,6 +26,10 @@ export const TodoList = () => {
 }
 
 // 役割と使い方を復習する
-// useRecoilValue
-// useRecoilState
-// useSetRecoilState
+// useRecoilValue : recoilにセットした値を呼び出すだけ。(読み取り専用)
+// useRecoilState : 
+// useStateと似ている、stateの取得・更新を行う。useStateと似ている
+// const [value , setValue] = userecioiState(any)
+// useStateとの違い。useStateは生の情報をそのまま扱う。useRecoilStateはAtomというデータストアを通してstateを扱うする。
+// useSetRecoilState : recoilに値をセットするだけ。レンダリングは行われない。
+// selector : Atomで管理されているstateを加工し、返す。
